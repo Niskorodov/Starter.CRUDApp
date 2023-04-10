1299ea15cb59d30dc33aab499a0f02991a3158cd
Dmitrii <103382610+Niskorodov@users.noreply.github.com>
2023-04-10 16:11:19 +0300

    Create Person

--- src/main/java/ru/niskorodov/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/niskorodov/springcourse/controllers/PeopleController.java
@@ -3,15 +3,15 @@ package ru.niskorodov.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.niskorodov.springcourse.dao.PersonDAO;
+import ru.niskorodov.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
     private final PersonDAO personDao;
+
     @Autowired
     public PeopleController(PersonDAO personDao) {
         this.personDao = personDao;
@@ -19,7 +19,7 @@ public class PeopleController {
 
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("people",personDao.index());
+        model.addAttribute("people", personDao.index());
         // Получим всех людей из DAO и передадим на отображение в представление
         return "/people/index";
     }
@@ -27,7 +27,18 @@ public class PeopleController {
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDao.show(id));
-         // Получим одного человека по id из DAO и передадим на отображение в представление
+        // Получим одного человека по id из DAO и передадим на отображение в представление
         return "/people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "/people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDao.save(person);
+        return "redirect:/people";
     }
 }
